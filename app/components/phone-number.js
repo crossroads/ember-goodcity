2439d57709c500885da5b0c07732c0948e009e73
namrataukirde <namrata.ukirde@kiprosh.com>
2017-08-03 15:58:09 +0530

    fix codeclimate empty block statment

--- app/components/phone-number.js
+++ app/components/phone-number.js
@@ -6,13 +6,17 @@ export default Ember.Component.extend({
   hidden: Ember.computed.empty("number"),
 
   displayNumber: Ember.computed('number', function(){
-    if (this.get("hidden")) {return ""}
+    if (this.get("hidden")) {
+      return "";
+    }
     var num = this.get("number").replace(/\+852/, "");
     return num.length > 4 ? num.substr(0, 4) + " " + num.substr(4) : num;
   }),
 
   linkNumber: Ember.computed('number', function(){
-    if (this.get("hidden")) {return ""}
+    if (this.get("hidden")) {
+      return "";
+    }
     var prefix = this.get("number").indexOf("+852") === -1 ? "+852" : "";
     return prefix + this.get("number").replace(/ /g, "");
   })
