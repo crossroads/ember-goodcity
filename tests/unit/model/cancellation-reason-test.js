1aa49810f2732bdd0c57084577a680a3cf243c48
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-15 12:48:22 +0530

    CancellationReason model test cases added

---
+++ tests/unit/model/cancellation-reason-test.js
@@ -0,0 +1,13 @@
+import { test, moduleForModel } from 'ember-qunit';
+import Ember from 'ember';
+
+moduleForModel('cancellation_reason', 'CancellationReason Model', {
+});
+
+test('check attributes', function(assert){
+  assert.expect(1);
+  var model = this.subject();
+  var name = Object.keys(model.toJSON()).indexOf('name') > -1;
+
+  assert.ok(name);
+});
