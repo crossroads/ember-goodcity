96cdf401f2f73790590ad89264e14ef0ec76b9dd
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-15 13:38:39 +0530

    Image model test cases added

--- tests/unit/model/holiday-test.js
+++ tests/unit/model/holiday-test.js
@@ -4,7 +4,7 @@ moduleForModel('holiday', 'Holiday Model', {
 });
 
 test('check attributes', function(assert){
-  assert.expect(1);
+  assert.expect(3);
   var model = this.subject();
   var name = Object.keys(model.toJSON()).indexOf('name') > -1;
   var year = Object.keys(model.toJSON()).indexOf('year') > -1;
