6240ce940cf34d3801ac52d03ff7e5bc7c5e40de
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-15 13:36:30 +0530

    Holiday model test cases added

---
+++ tests/unit/model/holiday-test.js
@@ -0,0 +1,16 @@
+import { test, moduleForModel } from 'ember-qunit';
+
+moduleForModel('holiday', 'Holiday Model', {
+});
+
+test('check attributes', function(assert){
+  assert.expect(1);
+  var model = this.subject();
+  var name = Object.keys(model.toJSON()).indexOf('name') > -1;
+  var year = Object.keys(model.toJSON()).indexOf('year') > -1;
+  var holiday = Object.keys(model.toJSON()).indexOf('holiday') > -1;
+
+  assert.ok(holiday);
+  assert.ok(year);
+  assert.ok(name);
+});
