84b258b423d2ddcb4c1d8077ee63e022ddd21867
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-18 11:18:29 +0530

    RejectionReason model test cases added

---
+++ tests/unit/model/rejection-reason-test.js
@@ -0,0 +1,24 @@
+import { test, moduleForModel } from 'ember-qunit';
+import Ember from 'ember';
+
+moduleForModel('rejection_reason', 'RejectionReason Model', {
+  needs: ['model:district']
+});
+
+test('check attributes', function(assert){
+  assert.expect(1);
+  var model = this.subject();
+  var name = Object.keys(model.toJSON()).indexOf('name') > -1;
+
+  assert.ok(name);
+});
+
+test('Relationships with other models', function(assert){
+  assert.expect(2);
+
+  var rejectionReason = this.store().modelFor('rejection_reason');
+  var relationshipsWithDistrict = Ember.get(rejectionReason, 'relationshipsByName').get('items');
+
+  assert.equal(relationshipsWithDistrict.key, 'items');
+  assert.equal(relationshipsWithDistrict.kind, 'hasMany');
+});
