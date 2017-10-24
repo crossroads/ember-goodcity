83e7f3cdac41e31f97d4f93ade5b44df67778883
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-24 10:01:28 +0530

    Deprecation warning fix: Contains replaced with includes

--- app/controllers/subscriptions.js
+++ app/controllers/subscriptions.js
@@ -186,7 +186,7 @@ export default Ember.Controller.extend({
 
     if (data.operation === "update" && !existingItem) {
       this.store.findRecord(type, item.id);
-    } else if (["create","update"].contains(data.operation)) {
+    } else if (["create","update"].includes(data.operation)) {
         var payload = {};
         payload[type] = item;
         this.store.pushPayload(payload);
