aab0060f8650b5d79b3c153cce86cb22352e94a5
viveksriram1 <vivek.sriram@kiprosh.com>
2017-12-12 13:21:29 +0530

    code refactor

--- addon/controllers/messages_base.js
+++ addon/controllers/messages_base.js
@@ -97,13 +97,14 @@ export default Ember.Controller.extend({
 
       var offerId = values.offer.id;
       var msg = values.body;
-      var beginIndex = msg.indexOf("[")+1;
-      var endIndex   = msg.indexOf("]");
-      var url_with_text = msg.slice(beginIndex, endIndex);
+      var url_with_text = msg.slice(msg.indexOf("[")+1, msg.indexOf("]"));
       var url_text_begin = url_with_text.indexOf("|");
       var url_text = url_with_text.slice(0, url_text_begin);
+      var url_for = url_with_text.slice(url_text_begin+1);
 
-      values.body = msg.replace("["+url_with_text+"]",`<a href='/offers/${offerId}/plan_delivery'>${url_text}</a>`);
+      if(url_for === 'transport_page'){
+        values.body = msg.replace("["+url_with_text+"]",`<a href='/offers/${offerId}/plan_delivery'>${url_text}</a>`);
+      }
 
       var message = this.store.createRecord("message", values);
       message.save()
