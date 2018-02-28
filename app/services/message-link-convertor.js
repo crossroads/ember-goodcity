7187430c210eb44f19d356623014af8f0eb1a307
viveksriram1 <vivek.sriram@kiprosh.com>
2018-02-28 16:39:01 +0530

    indentation

--- app/services/message-link-convertor.js
+++ app/services/message-link-convertor.js
@@ -7,7 +7,7 @@ export default Ember.Service.extend({
   convert: function(values) {
     var offerId = values.offer.id;
     var msg = values.body;
-    var url_with_text = msg.slice(msg.indexOf("[")+1, msg.indexOf("]"));
+    var url_with_text = msg.slice(msg.indexOf("[") + 1, msg.indexOf("]"));
     var url_text_begin = url_with_text.indexOf("|");
     var url_text = url_with_text.slice(0, url_text_begin);
     var url_for = url_with_text.slice(url_text_begin + 1);
