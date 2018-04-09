d01eafe4071e4b38234b529d1411ef6c3e23326a
namrataukirde <namrata.ukirde@kiprosh.com>
2018-04-09 17:54:44 +0530

    computed property changes for accessing roles

--- app/models/user_profile.js
+++ app/models/user_profile.js
@@ -15,10 +15,12 @@ export default Addressable.extend({
 
   roles: Ember.computed('userRoles.[]', function(){
     var roles = []
-    this.get('userRoles').forEach(userRole => {
-      roles.push(userRole.get('role'));
-    });
-    return roles;
+    if(this.get('userRoles.length')){
+      this.get('userRoles').forEach(userRole => {
+        roles.push(userRole.get('role'));
+      });
+      return roles;
+    }
   }),
 
   roleNames: Ember.computed('roles', function(){
@@ -29,33 +31,43 @@ export default Addressable.extend({
 
   permissionNames: Ember.computed('roles', function(){
     var permissionNames = []
-    this.get('roles').forEach(role => {
-      role.get('rolePermissions').forEach(rolePermision => {
-        permissionNames.push(rolePermision.get('permission.name'));
+    if(this.get('roles.length')){
+      this.get('roles').forEach(role => {
+        role.get('rolePermissions').forEach(rolePermision => {
+          permissionNames.push(rolePermision.get('permission.name'));
+        });
       });
-    });
-    return permissionNames;
+      return permissionNames;
+    }
   }),
 
   isReviewer: Ember.computed('roleNames', function(){
-    return this.get('roleNames').includes('Reviewer');
+    if(this.get('permissionNames.length')){
+      return this.get('roleNames').includes('Reviewer');
+    }
   }),
 
   isSupervisor: Ember.computed('roleNames', function(){
-    return this.get('roleNames').includes('Supervisor');
+    if(this.get('permissionNames.length')){
+      return this.get('roleNames').includes('Supervisor');
+    }
   }),
 
   canManageUsers: Ember.computed('permissionNames', function(){
-    return this.get('permissionNames').includes('can_manage_users');
+    if(this.get('permissionNames.length')){
+      return this.get('permissionNames').includes('can_manage_users');
+    }
   }),
 
   canManageHolidays: Ember.computed('permissionNames', function(){
-    return this.get('permissionNames').includes('can_manage_holidays');
+    if(this.get('permissionNames.length')){
+      return this.get('permissionNames').includes('can_manage_holidays');
+    }
   }),
 
   isDonor: Ember.computed.empty('roleNames'),
   // isDonor: Ember.computed.empty("permission.name"),
-  isStaff: Ember.computed.notEmpty("permission.name"),
+  // isStaff: Ember.computed.notEmpty("permission.name"),
   // isReviewer: Ember.computed.equal("permission.name", "Reviewer"),
   // isSupervisor: Ember.computed.equal("permission.name", "Supervisor"),
 
@@ -67,3 +79,4 @@ export default Addressable.extend({
     return (this.get('firstName') + " " + this.get('lastName'));
   })
 });
+
