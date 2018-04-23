bb7346269bd776a7753252a100a26372bfb928f7
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-04-23 10:04:01 +0530

    GCW:1841:- Disable item edit button if offer is received

--- app/components/online-button.js
+++ app/components/online-button.js
@@ -12,11 +12,15 @@ export default Ember.Component.extend(Ember.TargetActionSupport, {
   tagName: "button",
   attributeBindings: ["disabled"],
   disabled: false,
+  offer: null,
 
   updateDisabled: null,
   disabledOverride: false,
 
   didInsertElement: function() {
+    if(this.get("offer")) {
+      this.get("offer.state") === "received" ? this.set("disabled", true) : this.set("disabled", false);
+    }
     this.updateDisabled = Ember.run.bind(this, () => {
       var online = navigator.connection ? navigator.connection.type !== "none" : navigator.onLine;
       this.set("disabled", !online || this.get("disabledOverride"));
