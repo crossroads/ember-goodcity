00ddbdf385d3633d9e9c3b32bfadbfdb2e6eb242
namrataukirde <namrata.ukirde@kiprosh.com>
2018-04-27 16:06:09 +0530

    fix checkbox component for add - remove user roles

--- app/components/check-box.js
+++ app/components/check-box.js
@@ -14,6 +14,14 @@ export default Ember.Component.extend({
     }
   },
 
+  didReceiveAttrs() {
+    var isCurrentRole = this.get('selection').indexOf(parseInt(this.get('selected'))) >= 0;
+    if(isCurrentRole){77
+      this.$().prop('checked', true);
+      this.sendAction('action', this.get('value'), true);
+    }
+  },
+
   _updateElementValue: function() {
     this.set('checked', this.$().prop('checked'));
     var isChecked = this.get('checked')
