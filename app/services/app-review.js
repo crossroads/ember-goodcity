60a572f10fa4b5bf1cb2b69932bf5caba3c2f514
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-08-23 09:41:56 +0530

    Translation issue fixed

--- app/services/app-review.js
+++ app/services/app-review.js
@@ -8,27 +8,36 @@ export default Ember.Service.extend({
 
   promptReviewModal() {
     var _this = this;
+    var i18n = _this.get("i18n");
+    var rateTitle = i18n.t("app_review.title").string;
+    var rateMessage = i18n.t("app_review.message").string;
+    var rateCancelButtonLabel = i18n.t("app_review.cancel_button_label").string;
+    var rateLaterButtonLabel = i18n.t("app_review.later_button_label").string;
+    var rateButtonLabelName = i18n.t("app_review.rate_button_label").string;
+    var rateYesButtonLabel = i18n.t("app_review.yes_button_label").string;
+    var rateNoButtonLabel = i18n.t("app_review.no_button_label").string;
+    var rateAppRatePromptTitle = i18n.t("app_review.app_rate_prompt_title").string;
+    var rateFeedbackPromptTitle = i18n.t("app_review.feedback_prompt_title").string;
     if(_this.get("isMobileApp")) {
       AppRate.preferences = {
         displayAppName: config.APP.REVIEW_APP_NAME,
         usesUntilPrompt: 1,
         promptAgainForEachNewVersion: false,
         inAppReview: true,
-        useLanguage: _this.get("i18n.locale"),
         storeAppURL: {
           ios: config.APP.APPLE_APP_ID,
           android: config.APP.ANDROID_APP_URL,
         },
         customLocale: {
-          title: "Would you mind rating %@?",
-          message: "It won’t take more than a minute and helps to promote our app. Thanks for your support!",
-          cancelButtonLabel: "No, Thanks",
-          laterButtonLabel: "Remind Me Later",
-          rateButtonLabel: "Rate It Now",
-          yesButtonLabel: "Yes!",
-          noButtonLabel: "Not really",
-          appRatePromptTitle: 'Do you like using %@',
-          feedbackPromptTitle: 'Mind giving us some feedback?',
+          title: rateTitle,
+          message: rateMessage,
+          cancelButtonLabel: rateCancelButtonLabel,
+          laterButtonLabel: rateLaterButtonLabel,
+          rateButtonLabel: rateButtonLabelName,
+          yesButtonLabel: rateYesButtonLabel,
+          noButtonLabel: rateNoButtonLabel,
+          appRatePromptTitle: rateAppRatePromptTitle,
+          feedbackPromptTitle: rateFeedbackPromptTitle,
         },
         callbacks: {
           handleNegativeFeedback: function(){
