a83c1652cc7639c4194a90a46ed4fdcc9f88099b
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-08-23 09:32:54 +0530

    useLanguage added for chinese for apprate

--- app/services/app-review.js
+++ app/services/app-review.js
@@ -8,36 +8,27 @@ export default Ember.Service.extend({
 
   promptReviewModal() {
     var _this = this;
-    var i18n = _this.get("i18n");
-    var rateTitle = i18n.t("app_review.title");
-    var rateMessage = i18n.t("app_review.message");
-    var rateCancelButton = i18n.t("app_review.cancel_button_label");
-    var rateLaterButton = i18n.t("app_review.later_button_label");
-    var rateButtonLabel = i18n.t("app_review.rate_button_label");
-    var rateYesButtonLabel = i18n.t("app_review.yes_button_label");
-    var rateNoButtonLabel = i18n.t("app_review.no_button_label");
-    var appRateTitle = i18n.t("app_review.app_rate_prompt_title");
-    var feedBackTitle = i18n.t("app_review.feedback_prompt_title");
     if(_this.get("isMobileApp")) {
       AppRate.preferences = {
         displayAppName: config.APP.REVIEW_APP_NAME,
         usesUntilPrompt: 1,
         promptAgainForEachNewVersion: false,
         inAppReview: true,
+        useLanguage: _this.get("i18n.locale"),
         storeAppURL: {
           ios: config.APP.APPLE_APP_ID,
           android: config.APP.ANDROID_APP_URL,
         },
         customLocale: {
-          title: rateTitle,
-          message: rateMessage,
-          cancelButtonLabel: rateCancelButton,
-          laterButtonLabel: rateLaterButton,
-          rateButtonLabel: rateButtonLabel,
-          yesButtonLabel: rateYesButtonLabel,
-          noButtonLabel: rateNoButtonLabel,
-          appRatePromptTitle: appRateTitle,
-          feedbackPromptTitle: feedBackTitle,
+          title: "Would you mind rating %@?",
+          message: "It won’t take more than a minute and helps to promote our app. Thanks for your support!",
+          cancelButtonLabel: "No, Thanks",
+          laterButtonLabel: "Remind Me Later",
+          rateButtonLabel: "Rate It Now",
+          yesButtonLabel: "Yes!",
+          noButtonLabel: "Not really",
+          appRatePromptTitle: 'Do you like using %@',
+          feedbackPromptTitle: 'Mind giving us some feedback?',
         },
         callbacks: {
           handleNegativeFeedback: function(){
