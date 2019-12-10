909523678e2f2e16a1b3dc6b5797092bc3413a38
akshay gaikwad <akshay@kiprosh.com>
2019-12-10 17:11:49 +0530

    GCW-2853 added useDefault printer method

--- app/services/session.js
+++ app/services/session.js
@@ -1,6 +1,6 @@
-import Ember from 'ember';
-import '../computed/local-storage';
-import config from '../config/environment';
+import Ember from "ember";
+import "../computed/local-storage";
+import config from "../config/environment";
 
 export default Ember.Service.extend({
   authToken: Ember.computed.localStorage(),
@@ -10,17 +10,23 @@ export default Ember.Service.extend({
   seenTour: Ember.computed.localStorage(),
   store: Ember.inject.service(),
 
-  currentUser: Ember.computed(function(){
-    var store = this.get('store');
-    return store.peekAll('user_profile').get('firstObject') || null;
+  currentUser: Ember.computed(function() {
+    var store = this.get("store");
+    return store.peekAll("user_profile").get("firstObject") || null;
   }).volatile(),
 
-  isAdminApp: Ember.computed(function(){
+  userDefaultPrinter: function() {
+    console.log("hit");
+    const store = this.get("store");
+    return store.peekRecord("user", this.get("currentUser.id")).get("printer");
+  },
+
+  isAdminApp: Ember.computed(function() {
     return config.APP.NAME === "admin.goodcity";
   }),
 
-  isDonorApp: Ember.computed('isAdminApp', function(){
-    return this.get('isAdminApp') === false;
+  isDonorApp: Ember.computed("isAdminApp", function() {
+    return this.get("isAdminApp") === false;
   }),
 
   clear: function() {
