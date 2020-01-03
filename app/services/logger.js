8a092a5ef85a40ed91e62f67df4d40ea3e804b8b
Steve Kenworthy <steveyken@gmail.com>
2020-01-03 17:31:34 +0800

    Set correct environment.

--- app/services/logger.js
+++ app/services/logger.js
@@ -5,26 +5,24 @@ export default Ember.Service.extend({
   session: Ember.inject.service(),
   rollbar: Ember.inject.service(),
 
-  getReason(reason) {
-    return reason instanceof Error || typeof reason !== "object" ?
-      reason : JSON.stringify(reason);
+  notifyErrorCollector(reason) {
+    var currentUser = this.get("session.currentUser");
+    var userName = currentUser.get("fullName");
+    var userId = currentUser.get("id");
+    var error = reason instanceof Error || typeof reason !== "object" ?
+        reason : JSON.stringify(reason);
+    var environment = config.environment;
+    var version = config.APP.VERSION;
+    var appSHA = config.APP.SHA;
+    var appSharedSHA = config.APP.SHARED_SHA;
+    this.set('rollbar.currentUser', currentUser);
+    this.get('rollbar').error(error, { id: userId, username: userName, environment: environment,
+      version: version, appSHA: appSHA, appSharedSHA: appSharedSHA});
   },
 
-  error: function(reason) {
-    if (reason.status === 0) {
-      return;
-    }
+  error(reason) {
+    if (reason.status === 0) return;
     console.info(reason);
-    if (config.environment === "production" || config.staging) {
-      var data;
-      var currentUser = this.get("session.currentUser");
-      var userName = currentUser.get("fullName");
-      var userId = currentUser.get("id");
-      var error = this.getReason(reason);
-      var environment = config.staging ? "staging" : config.environment;
-      var version = `${config.APP.SHA} (shared ${config.APP.SHARED_SHA})`;
-      this.set('rollbar.currentUser', currentUser);
-      this.get('rollbar').error(error, data = { id: userId, username: userName, environment: environment });
-    }
+    this.notifyErrorCollector(reason);
   }
 });
