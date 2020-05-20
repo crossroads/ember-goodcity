02f7ec8b74e214246e43e78fdf53e6b457676430
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-05-20 16:13:01 +0530

    added visibleToDonor boolean field added

--- app/components/donor-conditions.js
+++ app/components/donor-conditions.js
@@ -1,14 +1,15 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
-  donorConditions: Ember.computed(function(){
-    return this.get('store').peekAll('donor_condition');
+  donorConditions: Ember.computed(function() {
+    return this.get("store")
+      .peekAll("donor_condition")
+      .filterBy("visibleToDonor", true);
   }),
 
-  sortedDonorConditions: Ember.computed('donorConditions.[]', function(){
-    return this.get('donorConditions').sortBy('id');
+  sortedDonorConditions: Ember.computed("donorConditions.[]", function() {
+    return this.get("donorConditions").sortBy("id");
   })
-
 });
