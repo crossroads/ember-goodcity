fea02edd4dee8abb44efdc4b492e0356bdacd5f1
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-09-16 17:27:17 +0530

    removed windows-compat.js reference in index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* jshint node: true */
-'use strict';
+"use strict";
 
 module.exports = {
-  name: 'shared-goodcity',
+  name: "shared-goodcity",
   isDevelopingAddon: function() {
     return true;
   },
@@ -11,20 +11,36 @@ module.exports = {
     this._super.included(app);
 
     app.import("bower_components/font-awesome/css/font-awesome.css");
-    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
-    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", { destDir: "fonts" });
-    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: "fonts" });
-    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
-    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff2", { destDir: "fonts" });
-    app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", {
+      destDir: "fonts"
+    });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.svg", {
+      destDir: "fonts"
+    });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", {
+      destDir: "fonts"
+    });
+    app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", {
+      destDir: "fonts"
+    });
+    app.import(
+      "bower_components/font-awesome/fonts/fontawesome-webfont.woff2",
+      { destDir: "fonts" }
+    );
+    app.import("bower_components/font-awesome/fonts/FontAwesome.otf", {
+      destDir: "fonts"
+    });
   },
 
   contentFor: function(type, config) {
-    if (type === 'head-footer' && config.cordova.enabled) {
+    if (type === "head-footer" && config.cordova.enabled) {
       // add content security policy meta tag wanted by cordova-plugin-whitelist
       // add winstore-jscompat.js for windows phone (not needed for Windows 10) https://github.com/MSOpenTech/winstore-jscompat
-      return '<meta http-equiv="Content-Security-Policy" content="' + this.getContentPolicy(config) + '">' +
-        '\n<script src="shared.goodcity/winstore-jscompat.js"></script>';
+      return (
+        '<meta http-equiv="Content-Security-Policy" content="' +
+        this.getContentPolicy(config) +
+        '">'
+      );
     }
   },
 
@@ -36,11 +52,14 @@ module.exports = {
     };
 
     Object.keys(cordovaPolicy).forEach(function(key) {
-      config.contentSecurityPolicy[key] = (config.contentSecurityPolicy[key] || "") + " " + cordovaPolicy[key];
+      config.contentSecurityPolicy[key] =
+        (config.contentSecurityPolicy[key] || "") + " " + cordovaPolicy[key];
     });
 
     return Object.keys(config.contentSecurityPolicy)
-      .map(function(key) { return key + " " + config.contentSecurityPolicy[key]; })
+      .map(function(key) {
+        return key + " " + config.contentSecurityPolicy[key];
+      })
       .join("; ");
   }
 };
