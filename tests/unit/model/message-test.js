430ef7df02a6065ac16ea6f9bae1f43a3652d09d
AbulAsar Sayyad <abul.a@kiprosh.com>
2020-09-22 18:55:51 +0530

    fix failing tests

--- tests/unit/model/message-test.js
+++ tests/unit/model/message-test.js
@@ -1,18 +1,18 @@
-import { test, moduleForModel } from 'ember-qunit';
-import Ember from 'ember';
+import { test, moduleForModel } from "ember-qunit";
+import Ember from "ember";
 
-moduleForModel('message', 'Message Model', {
-  needs: ['model:user', 'model:item', 'model:offer']
+moduleForModel("message", "Message Model", {
+  needs: ["model:item", "model:offer", "model:user"]
 });
 
-test('check attributes', function(assert){
+test("check attributes", function(assert) {
   assert.expect(5);
   var model = this.subject();
-  var state = Object.keys(model.toJSON()).indexOf('state') > -1;
-  var updatedAt = Object.keys(model.toJSON()).indexOf('updatedAt') > -1;
-  var createdAt = Object.keys(model.toJSON()).indexOf('createdAt') > -1;
-  var isPrivate = Object.keys(model.toJSON()).indexOf('isPrivate') > -1;
-  var body = Object.keys(model.toJSON()).indexOf('body') > -1;
+  var state = Object.keys(model.toJSON()).indexOf("state") > -1;
+  var updatedAt = Object.keys(model.toJSON()).indexOf("updatedAt") > -1;
+  var createdAt = Object.keys(model.toJSON()).indexOf("createdAt") > -1;
+  var isPrivate = Object.keys(model.toJSON()).indexOf("isPrivate") > -1;
+  var body = Object.keys(model.toJSON()).indexOf("body") > -1;
 
   assert.ok(body);
   assert.ok(isPrivate);
@@ -21,20 +21,26 @@ test('check attributes', function(assert){
   assert.ok(state);
 });
 
-test('Relationships with other models', function(assert){
+test("Relationships with other models", function(assert) {
   assert.expect(6);
 
-  var message = this.store().modelFor('message');
-  var relationshipsWithItem = Ember.get(message, 'relationshipsByName').get('item');
-  var relationshipsWithUser = Ember.get(message, 'relationshipsByName').get('sender');
-  var relationshipsWithOffer = Ember.get(message, 'relationshipsByName').get('offer');
+  var message = this.store().modelFor("message");
+  var relationshipsWithItem = Ember.get(message, "relationshipsByName").get(
+    "item"
+  );
+  var relationshipsWithUser = Ember.get(message, "relationshipsByName").get(
+    "sender"
+  );
+  var relationshipsWithOffer = Ember.get(message, "relationshipsByName").get(
+    "offer"
+  );
 
-  assert.equal(relationshipsWithItem.key, 'item');
-  assert.equal(relationshipsWithItem.kind, 'belongsTo');
+  assert.equal(relationshipsWithItem.key, "item");
+  assert.equal(relationshipsWithItem.kind, "belongsTo");
 
-  assert.equal(relationshipsWithUser.key, 'sender');
-  assert.equal(relationshipsWithUser.kind, 'belongsTo');
+  assert.equal(relationshipsWithUser.key, "sender");
+  assert.equal(relationshipsWithUser.kind, "belongsTo");
 
-  assert.equal(relationshipsWithOffer.key, 'offer');
-  assert.equal(relationshipsWithOffer.kind, 'belongsTo');
+  assert.equal(relationshipsWithOffer.key, "offer");
+  assert.equal(relationshipsWithOffer.kind, "belongsTo");
 });
--- tests/unit/model/user-profile-test.js
+++ tests/unit/model/user-profile-test.js
@@ -1,18 +1,19 @@
-import { test, moduleForModel } from 'ember-qunit';
+import { test, moduleForModel } from "ember-qunit";
 // import Ember from 'ember';
 
-moduleForModel('user_profile', 'UserProfile Model', {
-  needs: ['model:permission', 'model:user', 'model:address', 'model:user-role']
+moduleForModel("user_profile", "UserProfile Model", {
+  needs: ["model:permission", "model:address", "model:user-role"]
 });
 
-test('check attributes', function(assert){
+test("check attributes", function(assert) {
   assert.expect(5);
   var model = this.subject();
-  var firstName = Object.keys(model.toJSON()).indexOf('firstName') > -1;
-  var lastName = Object.keys(model.toJSON()).indexOf('lastName') > -1;
-  var mobile = Object.keys(model.toJSON()).indexOf('mobile') > -1;
-  var donationAmount = Object.keys(model.toJSON()).indexOf('donationAmount') > -1;
-  var donationDate = Object.keys(model.toJSON()).indexOf('donationDate') > -1;
+  var firstName = Object.keys(model.toJSON()).indexOf("firstName") > -1;
+  var lastName = Object.keys(model.toJSON()).indexOf("lastName") > -1;
+  var mobile = Object.keys(model.toJSON()).indexOf("mobile") > -1;
+  var donationAmount =
+    Object.keys(model.toJSON()).indexOf("donationAmount") > -1;
+  var donationDate = Object.keys(model.toJSON()).indexOf("donationDate") > -1;
 
   assert.ok(donationDate);
   assert.ok(donationAmount);
