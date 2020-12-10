267d570046590595a5ec4568c088b5898b67c8fe
bharat619 <bharat.h@kiprosh.com>
2020-12-10 16:19:48 +0530

    Use const

--- app/utils/polyfill.js
+++ app/utils/polyfill.js
@@ -9,7 +9,7 @@ export const __get = (obj, path, defaultValue) => {
   if (typeof path !== string) {
     throw Error("Invalid argument passed");
   }
-  _path = path.split(".").filter(key => key.length);
+  const _path = path.split(".").filter(key => key.length);
   const value = _path.reduce((acc, key) => acc && acc[key], obj);
   return value || defaultValue;
 };
