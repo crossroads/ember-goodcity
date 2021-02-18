c374234fc5d9c157e1991e5209b02093d5545a0a
abulsayyad123 <abul.a@kiprosh.com>
2021-02-18 16:25:27 +0530

    GCW-3508-Added partial for global component

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,9 @@
-import config from '../config/environment';
+import config from "../config/environment";
 
 export default Ember.Controller.extend({
-
   cordova: Ember.inject.service(),
   subscriptions: Ember.inject.controller(),
+  messageService: Ember.inject.service(),
   isMobileApp: config.cordova.enabled,
   config,
 
@@ -12,14 +12,16 @@ export default Ember.Controller.extend({
   appTitle: config.APP.TITLE,
   bannerImage: config.APP.BANNER_IMAGE,
 
-  initSubscriptions: Ember.on('init', function() {
+  initSubscriptions: Ember.on("init", function() {
     if (this.session.get("isLoggedIn")) {
-      this.send('setSubscriptions');
+      this.send("setSubscriptions");
     }
   }),
 
-  supportGCLink: Ember.computed('session.language', function() {
-    return this.get('session.language') === 'zh-tw' ? "https://www.crossroads.org.hk/zh-hant/home/donate-funds/" : "https://www.crossroads.org.hk/home/donate-funds/";
+  supportGCLink: Ember.computed("session.language", function() {
+    return this.get("session.language") === "zh-tw"
+      ? "https://www.crossroads.org.hk/zh-hant/home/donate-funds/"
+      : "https://www.crossroads.org.hk/home/donate-funds/";
   }),
 
   appVersion: Ember.computed(function() {
@@ -29,27 +31,27 @@ export default Ember.Controller.extend({
   actions: {
     logMeOut() {
       this.session.clear(); // this should be first since it updates isLoggedIn status
-      this.get('subscriptions').send('unwire');
-      this.get('subscriptions').send('unloadNotifications');
+      this.get("subscriptions").send("unwire");
+      this.get("subscriptions").send("unloadNotifications");
       this.store.unloadAll();
       var _this = this;
       config.APP.PRELOAD_TYPES.forEach(function(type) {
         _this.store.findAll(type);
       });
-      this.transitionToRoute('login');
+      this.transitionToRoute("login");
     },
 
     logMeIn() {
-      this.send('setSubscriptions');
+      this.send("setSubscriptions");
     },
 
     setSubscriptions() {
-      this.get('subscriptions').send('wire');
+      this.get("subscriptions").send("wire");
     },
 
     rateApp() {
       if (this.get("cordova").isIOS()) {
-        this.set("app_id", this.get('ios_app_id'));
+        this.set("app_id", this.get("ios_app_id"));
       }
       LaunchReview.launch(this.get("app_id"));
     }
