fe60922b5ebb38a5845a0a2a2778f685155579ab
shreyas098 <shreyash@kiprosh.com>
2021-08-18 01:11:04 +0530

    Add general text message for feedback form

--- app/services/message-link-convertor.js
+++ app/services/message-link-convertor.js
@@ -1,4 +1,4 @@
-import Ember from 'ember';
+import Ember from "ember";
 const { getOwner } = Ember;
 
 export default Ember.Service.extend({
@@ -12,8 +12,18 @@ export default Ember.Service.extend({
     var url_text = url_with_text.slice(0, url_text_begin);
     var url_for = url_with_text.slice(url_text_begin + 1);
 
-    if(url_for === 'transport_page'){
-      values.body = msg.replace("["+url_with_text+"]", `<a href='/offers/${offerId}/plan_delivery'>${url_text}</a>`);
+    if (url_for === "transport_page") {
+      values.body = msg.replace(
+        "[" + url_with_text + "]",
+        `<a href='/offers/${offerId}/plan_delivery'>${url_text}</a>`
+      );
+    }
+
+    if (url_for === "feedback_form") {
+      values.body = msg.replace(
+        "[" + url_with_text + "]",
+        `<a href=' https://crossroads-foundation.formstack.com/forms/goodcity_feedback?OfferId=${offerId}'>${url_text}</a>`
+      );
     }
   }
 });
