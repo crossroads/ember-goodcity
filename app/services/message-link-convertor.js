a16d9fb28349a198ccc6c4157215a23b6ba0e11a
shreyas098 <shreyash@kiprosh.com>
2021-08-24 21:37:19 +0530

    Fix HTML unescape issue for messages

--- app/services/message-link-convertor.js
+++ app/services/message-link-convertor.js
@@ -6,14 +6,15 @@ export default Ember.Service.extend({
 
   convert: function(values) {
     var offerId = values.offer.id;
-    var msg = values.body.trim();
-    msg = Ember.Handlebars.Utils.escapeExpression(msg || "");
-    msg = msg.replace(/(\r\n|\n|\r)/gm, "<br>");
+    values.body = values.body.trim();
+    values.body = Ember.Handlebars.Utils.escapeExpression(values.body || "");
+    values.body = values.body.replace(/(\r\n|\n|\r)/gm, "<br>");
+    var msg = values.body;
 
     var url_with_text = msg.slice(msg.indexOf("[") + 1, msg.indexOf("]"));
     var url_text_begin = url_with_text.indexOf("|");
     var url_text = url_with_text.slice(0, url_text_begin);
-    var url_for = url_with_text.slice(url_text_begin + 1);
+    var url_for = url_with_text.slice(url_text_begin + 1).trim();
 
     if (url_for === "transport_page") {
       values.body = msg.replace(
