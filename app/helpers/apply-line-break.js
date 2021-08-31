b8b3631fac0ef0159095a6a300d9619c512605e2
shreyas098 <shreyash@kiprosh.com>
2021-08-31 22:50:27 +0530

    Add escapeExpression to the apply line helper

--- app/helpers/apply-line-break.js
+++ app/helpers/apply-line-break.js
@@ -3,5 +3,7 @@ import Ember from "ember";
 export default Ember.Helper.helper(function(value) {
   value = new Ember.Handlebars.SafeString(value[0] || "").string;
   value = value.replace(/(\r\n|\n|\r)/gm, "<br>");
-  return new Ember.String.htmlSafe(value);
+  return new Ember.String.htmlSafe(
+    Ember.Handlebars.Utils.escapeExpression(value)
+  );
 });
